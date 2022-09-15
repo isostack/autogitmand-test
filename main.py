52c51224c723935c33eaa2289016d422d32a288d
Isostack <dezynarh@gmail.com>
2022-09-15 01:01:51 +0100

    Project Update

---
+++ main.py
@@ -0,0 +1,16 @@
+import autogitmand
+import requests
+import datetime
+
+today = datetime.datetime.now()
+today = today.strftime("%Y-%m-%d")
+
+response = requests.get('https://www.bbc.com/')
+
+webpage = response.text
+
+#with open(f'{today}.html', 'w') as f:
+#    f.write(webpage)
+
+autogitmand.start('.')
+#autogitmand.start(f'{today}.html')
